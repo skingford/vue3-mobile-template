3ef14b55f07872bcaea3b88a54cc9e9eca06fff3
SteveKingFord <332312568@qq.com>
2021-07-11 09:51:46 +0800

    chore: 安装sass

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 /*
  * @Author: kingford
  * @Date: 2021-06-13 01:43:14
- * @LastEditTime: 2021-07-10 22:22:34
+ * @LastEditTime: 2021-07-11 09:49:52
  */
 import { createApp } from 'vue';
 import { router } from './router';
 //import App from './App.vue'
 import App from './App';
 import storage from '@utils/storage';
+
+// global styles
+import 'normalize.css/normalize.css';
+import './styles/index.scss';
 
 const app = createApp(App);
 

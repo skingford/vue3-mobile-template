126e2d5557245f1a60b3d5c70aeef34e7a1722a1
kingford <32887862+skingford@users.noreply.github.com>
2021-09-24 18:04:14 +0800

    feat: 目录

--- build/vite/plugin/vconsole.ts
+++ build/vite/plugin/vconsole.ts
@@ -1,14 +1,16 @@
 /*
  * @Author: kingford
  * @Date: 2021-09-24 17:53:13
- * @LastEditTime: 2021-09-24 17:58:11
+ * @LastEditTime: 2021-09-24 18:03:23
  */
 import { viteVConsole } from 'vite-plugin-vconsole';
 import { resolve } from 'path';
 
+const ENTRY_PATH = resolve(process.cwd(), 'src/main.ts');
+
 export function vconsolePlugin() {
   const vconsolePlugin = viteVConsole({
-    entry: resolve('src/main.ts'),
+    entry: ENTRY_PATH,
     localEnabled: true,
     enabled: true,
     config: {
